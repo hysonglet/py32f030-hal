8b23409263e6b524b74996f4468df3bf2e726862
hysonglet <hysonglet@qq.com>
2024-07-23 19:18:59 +0800

    change i2c

--- src/i2c/master.rs
+++ src/i2c/master.rs
@@ -25,3 +25,35 @@ impl<'d, T: I2cInstance> Master<'d, T, Blocking> {
         T::master_receive_block(address, buf)
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+impl<'d, T: I2cInstance> embedded_hal::blocking::i2c::Write for Master<'d, T, Blocking> {
+    type Error = Error;
+    fn write(&mut self, address: u8, bytes: &[u8]) -> Result<(), Self::Error> {
+        self.write_block(address, bytes)
+            .map_or_else(|e| Err(e), |_| Ok(()))
+    }
+}
+
+impl<'d, T: I2cInstance> embedded_hal::blocking::i2c::Read for Master<'d, T, Blocking> {
+    type Error = Error;
+    fn read(&mut self, address: u8, buffer: &mut [u8]) -> Result<(), Self::Error> {
+        self.read_block(address, buffer)
+            .map_or_else(|e| Err(e), |_| Ok(()))
+    }
+}
+
+impl<'d, T: I2cInstance> embedded_hal::blocking::i2c::WriteRead for Master<'d, T, Blocking> {
+    type Error = Error;
+    fn write_read(
+        &mut self,
+        address: u8,
+        bytes: &[u8],
+        buffer: &mut [u8],
+    ) -> Result<(), Self::Error> {
+        self.write_block(address, bytes)
+            .map_or_else(|e| Err(e), |_| Ok(()))?;
+        self.read_block(address, buffer)
+            .map_or_else(|e| Err(e), |_| Ok(()))
+    }
+}
