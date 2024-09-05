b345debd8f133082a90c286da2a4b056d0c3335e
彭忠怀 <hysonglet@qq.com>
2024-09-05 22:24:33 +0800

    Add crc

---
+++ src/crc/hal.rs
@@ -0,0 +1,24 @@
+pub(super) mod sealed {
+    use super::super::*;
+    use crate::pac;
+
+    pub trait Instance {
+        #[inline]
+        fn block() -> &'static pac::crc::RegisterBlock {
+            unsafe { pac::CRC::PTR.as_ref().unwrap() }
+        }
+
+        #[inline]
+        fn reset() {
+            Self::block().cr.write(|w| w.reset().set_bit())
+        }
+
+        fn read_data() -> u32 {
+            Self::block().dr.read().bits()
+        }
+
+        fn write_data(v: u32) {
+            Self::block().dr.write(|w| unsafe { w.dr().bits(v) })
+        }
+    }
+}
--- src/crc/mod.rs
+++ src/crc/mod.rs
@@ -1,1 +1,44 @@
+use crate::clock::peripheral::PeripheralClockIndex;
+use core::marker::PhantomData;
+use embassy_hal_internal::{into_ref, Peripheral};
+mod hal;
 
+pub trait Instance: Peripheral<P = Self> + hal::sealed::Instance + 'static + Send {}
+
+impl Instance for crate::mcu::peripherals::CRC {}
+impl hal::sealed::Instance for crate::mcu::peripherals::CRC {}
+
+pub struct Crc<'d, T: Instance> {
+    _t: PhantomData<&'d T>,
+}
+
+impl<'d, T: Instance> Crc<'d, T> {
+    pub fn new(_crc: impl Peripheral<P = T>) -> Self {
+        into_ref!(_crc);
+
+        PeripheralClockIndex::CRC.clock(true);
+        T::reset();
+
+        Self { _t: PhantomData }
+    }
+
+    pub fn accumulat(&self, buf: &[u32]) -> u32 {
+        buf.iter().for_each(|v| T::write_data(*v));
+        T::read_data()
+    }
+
+    pub fn calculate(&self, buf: &[u32]) -> u32 {
+        T::reset();
+        self.accumulat(buf)
+    }
+
+    pub fn reset(&self) {
+        T::reset()
+    }
+}
+
+impl<'d, T: Instance> Drop for Crc<'d, T> {
+    fn drop(&mut self) {
+        PeripheralClockIndex::CRC.clock(false);
+    }
+}
